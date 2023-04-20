d2da12dea4ffb785a5d6107543ed50c8fb815707
wangzh <>
2023-04-20 21:19:33 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // })
-export default defineConfig({
+module.exports = defineConfig({
   transpileDependencies: true,
-  resolve: {
-    alias: {
-      '@': resolve('src'), // 源码根目录
-      '@img': resolve('src/assets/img'), // 图片
-      '@less': resolve('src/assets/less'), // 预处理器
-      '@libs': resolve('src/libs'), // 本地库
-      '@plugins': resolve('src/plugins'), // 本地插件
-      '@components': resolve('src/components'), // 公共组件
-      '@views': resolve('src/views'), // 路由组件
+  configureWebpack: { // 别名配置
+    resolve: {
+      alias: {
+        src: '@', // 源码根目录
+        img: '@/assets/img',  // 图片
+        less: '@/assets/less',  // 预处理器
+        libs: '@/libs', // 本地库
+        plugins: '@/plugins', // 本地插件
+        components: '@/components', // 公共组件
+        views: '@/views', // 路由组件
+      },
     },
-  },
+  }
 })
